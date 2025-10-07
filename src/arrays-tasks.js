1e7959229e3e4904b9587f2c2f65a683ff5165e9
Ivan <vaniamar@mail.ru>
2025-10-07 18:51:34 +0700

    add: test solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -101,8 +101,11 @@ function removeFalsyValues(/* arr */) {
  *    getStringsLength([ '', 'a', 'bc', 'def', 'ghij' ]) => [ 0, 1, 2, 3, 4 ]
  *    getStringsLength([ 'angular', 'react', 'ember' ]) => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength(arr) {
+  const result = arr.map((item) => {
+    return item.length;
+  });
+  return result;
 }
 
 /**
